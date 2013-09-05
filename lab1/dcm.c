874c0313004873613a1184554d0780fc54244462
Pikafu <sf323@cornell.edu>
2013-09-04 23:40:35 -0400

    Added lab1 files.

---
+++ lab1/dcm.c
@@ -0,0 +1,101 @@
+/* This is John and Michael. We are awesome! */
+
+#define F_CPU 16000000UL //GO CLOCK GO!
+
+#include <inttypes.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <avr/pgmspace.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <util/delay.h> // needed for lcd_lib
+#include "lcd_lib.h"
+#include "uart.h"
+
+#define t1 200  
+
+int8_t lcd_buffer[17];	// LCD display buffer
+const int8_t LCD_initialize[] PROGMEM = "LCD Initialized\0";
+const int8_t LCD_number[] PROGMEM = "Cap=\0";
+uint8_t anipos, dir;	// move a character around  
+uint16_t LCD_capacitance;
+
+// timeout counter for task1
+volatile unsigned char time1; 
+
+// timer 1 capture variables for computing sq wave period	
+volatile unsigned int T1capture;
+
+// Capacitance in nF
+volatile unsigned int capacitance;
+
+
+
+void initialize(void); // Set up the MCU
+
+
+int main(void)
+{
+	initialize();
+	if (time1==0){time1=t1;	task1();}
+
+}
+void blinkLEDs(void); // Blink some LEDs for testing
+void task1(void)
+{
+	DDRB=(0<<DDB3);	// Port b.3 is an input and port b.2
+	DDRB=(1<<DDB2); // is an output
+	
+	//Compute Capacitance
+	capacitance = T1capture;///(11207.4); //amount of time for charging capacitance to comparator switch
+									  // Assuming R3/R3 + R4 ratio is 0.6 and the capicitance is between 1 nF and 100 nF
+	//Update LCD	
+	sprintf(lcd_buffer,"%-i",capacitance);
+	LCDGotoXY(4, 0);
+  	// display the count 
+	LCDstring(lcd_buffer, strlen(lcd_buffer));	
+	CopyStringtoLCD(LCD_capacitance, 0, 0);
+
+	DDRB=(0<<DDB2); // Set port b.2 input to charge cap
+}
+void initialize(void) 
+{
+	TCCR0A = (1<<WGM01);	          // Set WGM bits to 010 to set desired mode (CTC mode, pg. 106)
+	TCCR0B = (1<<CS01)|(1<<CS00); // Bits 0 and 1. Set timer 0 prescaler to 64
+	TIMSK0 = (1<<OCIE0A); 		  // Bit 1: Enable compare match for timer0 interrupts
+	OCR0A = 249;				  // Set the compare register to 250 time ticks
+	
+	// Set up timer1 for full speed
+	TCCR1B = (1<<ICES1)|(1<<CS00);  // Set capture to positive edge, and set timer1 for full speed
+	TIMSK1 = (1<<ICIE1);                    // Turn on timer1 interrupt-on-capture
+
+	ACSR = (1<<ACIC) ; // Set analog comp to connect to timer capture input
+	DDRB = 0;          // Comparator negative input is B.3
+
+	//LCD init
+	init_lcd();
+	LCDclr();
+}
+
+void init_lcd(void) 
+{
+	LCDinit();	//initialize the display
+	LCDcursorOFF();
+	LCDclr();				//clear the display
+	LCDGotoXY(0,0);
+	CopyStringtoLCD(LCD_initialize, 0, 0);
+}	
+
+//Timer 0 overflow ISR
+ISR (TIMER0_COMPA_vect) 
+{
+  if (time1>0)	--time1;
+}
+//Timer 1 capture ISR
+ISR (TIMER1_CAPT_vect) 
+{
+	T1capture = ICR1;
+
+} 
